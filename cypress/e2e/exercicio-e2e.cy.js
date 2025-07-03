9d7ddfb2670f1a75e73c7a99c201d362a6b6fc06
Kauan Rosa <kauan220017@gmail.com>
2025-07-03 15:31:03 -0300

    Melhoria na atividade de teste-e2e

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,5 +1,5 @@
 /// <reference types="cypress" />
-import {faker} from '@faker-js/faker'
+import { faker } from '@faker-js/faker'
 
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
   /*  Como cliente 
@@ -17,27 +17,24 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
   it('Fluxo de teste parte 1', () => {
       //TODO: Coloque todo o fluxo de teste aqui, considerando as boas práticas e otimizações
       
-        const email = faker.internet.email()
-        const password = faker.internet.password()
+        
         const name = faker.person.firstName()
         const lastName = faker.person.lastName()
         const location = faker.location.streetAddress()
-        cy.get('#reg_email').type(email)
-        cy.get('#reg_password').type(password)
-        cy.get(':nth-child(4) > .button').click()
-        cy.get('.woocommerce-MyAccount-content').should('contain', 'Olá')
+        cy.registrarUsuarioFaker()
         cy.visit('produtos')
-        cy.get('.page-title').should('contain', 'Produtos')
-        cy.get('.post-3374 > .product-block > .block-inner > .image > .product-image > .image-hover').click()
-        cy.get('.button-variable-item-32').click()
-        cy.get(':nth-child(2) > .value > .variable-items-wrapper > .variable-item').click()
-        cy.get('.single_add_to_cart_button').click()
-        cy.get('.search').type('Ariel Roll Sleeve Sweatshirt{enter}')
+        cy.get('.search').type('Arcadio Gym Short{enter}')
         cy.get('.search > .tbay-search-form > .form-ajax-search > .form-group > .input-group > .button-group').type('{enter}')
-        cy.get('.button-variable-item-XS').click()
-        cy.get('.button-variable-item-Purple').click()
+        cy.get('.post-3528 > .product-block').click()
+        cy.get('.button-variable-item-33').click()
+        cy.get('.button-variable-item-Blue').click()
         cy.get('.single_add_to_cart_button').click()
-        cy.get('.search').type('Josie Yoga Jacket{enter}')
+        cy.get('.search').type('Stellar Solar Jacket{enter}')
+        cy.get('.search > .tbay-search-form > .form-ajax-search > .form-group > .input-group > .button-group').type('{enter}')
+        cy.get('.button-variable-item-S').click()
+        cy.get('.button-variable-item-Blue').click()
+        cy.get('.single_add_to_cart_button').click()
+        cy.get('.search').type('Augusta Pullover Jacket{enter}')
         cy.get('.search > .tbay-search-form > .form-ajax-search > .form-group > .input-group > .button-group').type('{enter}')
         cy.get('.button-variable-item-S').click()
         cy.get('.button-variable-item-Blue').click()
@@ -45,27 +42,21 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         cy.get('.search').type('Autumn Pullie{enter}')
         cy.get('.search > .tbay-search-form > .form-ajax-search > .form-group > .input-group > .button-group').type('{enter}')
         cy.get('.button-variable-item-XS').click()
-        cy.get('.button-variable-item-Green').click()
+        cy.get('.button-variable-item-Purple').click()
         cy.get('.single_add_to_cart_button').click()
         cy.get('.dropdown-toggle > .text-skin > .icon-basket').click()
         cy.get('#cart > .dropdown-menu > .widget_shopping_cart_content > .mini_cart_content > .mini_cart_inner > .mcart-border > .buttons > .checkout').click()
-        cy.get('#billing_first_name').type(name).click()
-        cy.get('#billing_last_name').type(lastName).click()
-        cy.get('#billing_address_1').type(location).click()
-        cy.get('#billing_city').type('São José').click()
-        cy.get('#billing_postcode').type('88102-001').click()
-        cy.get('#billing_phone').type('48999999999').click()
-        cy.get('#terms').click()
-        cy.get('#place_order').click()
-
-
-  });
-
+        cy.preencherCheckoutFaker()
+        
+        });
 });
 
  
-    
-      
+
+ 
+
+
+
   
 
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,40 @@
-Cypress.Commands.add('login', (usuario, senha) => {
-    cy.get('#username').type(usuario)
-    cy.get('#password').type(senha, {log: false})
-    cy.get('.woocommerce-form > .button').click()
-});
+/// <reference types="cypress" />
+import { faker } from '@faker-js/faker'
+
+// Comando customizado para registrar usuário com dados aleatórios
+Cypress.Commands.add('registrarUsuarioFaker', () => {
+  const email = faker.internet.email()
+  const password = faker.internet.password()
+
+  cy.get('#reg_email').type(email)
+  cy.get('#reg_password').type(password)
+  cy.get(':nth-child(4) > .button').click()
+
+})
+
+Cypress.Commands.add('preencherCheckoutFaker', () => {
+  const firstName = faker.person.firstName()
+  const lastName = faker.person.lastName()
+  const address = faker.location.streetAddress()
+  const city = 'São José'
+  const cep = '88102-001'
+  const telefone = '48999999999'
+
+  cy.get('#billing_first_name').type(firstName)
+  cy.get('#billing_last_name').type(lastName)
+  cy.get('#billing_address_1').type(address)
+  cy.get('#billing_city').type(city)
+  cy.get('#billing_postcode').type(cep)
+  cy.get('#billing_phone').type(telefone)
+  cy.get('#terms').click()
+  cy.get('#place_order').click()
+
+  cy.wait(8000)
+  cy.get('.woocommerce-order').should('contain', 'Obrigado. Seu pedido foi recebido.')
+})
+
+
+
+
+
 
